eeba2892fd625b47780767813b6b90d7830fcb62
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-10-22 12:55:52 -0500

    Se crearon las clases u objetos para hacer la lectura del Json Server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeadingComponentComponent } from './heading.component/heading.component.component';
+import { AsignaturasComponentComponent } from './asignaturas.component/asignaturas.component.component';
+import { PerfilComponentComponent } from './perfil.component/perfil.component.component';
+import { AddQuestionComponentComponent } from './add-question.component/add-question.component.component';
+import { HomeComponentComponent } from './home.component/home.component.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AgregarAsignaturaComponentComponent } from './agregar-asignatura.component/agregar-asignatura.component.component';
+import { PerfilBarraComponentComponent } from './perfil-barra.component/perfil-barra.component.component';
+import { BuscarComponent } from './buscar/buscar.component';
+import { AgregarRespuestaComponent } from './agregar-respuesta/agregar-respuesta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    HeadingComponentComponent,
+    AsignaturasComponentComponent,
+    PerfilComponentComponent,
+    AddQuestionComponentComponent,
+    HomeComponentComponent,
+    AgregarAsignaturaComponentComponent,
+    PerfilBarraComponentComponent,
+    BuscarComponent,
+    AgregarRespuestaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
