c2f7097a531bdbd3c9a39e813910f72e0e3f3833
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-10-22 12:52:05 -0500

    Se crearon las rutas de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddQuestionComponentComponent } from './add-question.component/add-question.component.component';
+import { AgregarAsignaturaComponentComponent } from './agregar-asignatura.component/agregar-asignatura.component.component';
+import { AgregarRespuestaComponent } from './agregar-respuesta/agregar-respuesta.component';
+import { BuscarComponent } from './buscar/buscar.component';
+import { HomeComponentComponent } from './home.component/home.component.component';
+import { PerfilComponentComponent } from './perfil.component/perfil.component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponentComponent},
+  {path:'home',component:HomeComponentComponent},
+  {path: 'add_pregunta', component:AddQuestionComponentComponent},
+  {path:'add-asignatura', component:AgregarAsignaturaComponentComponent},
+  {path:'perfil',component:PerfilComponentComponent},
+  {path:'buscar',component:BuscarComponent},
+  {path:'agregar_respuesta', component:AgregarRespuestaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
